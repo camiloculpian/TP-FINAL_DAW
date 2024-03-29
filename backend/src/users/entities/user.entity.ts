dc975161c1cdb038b92d58e22e7ec57177f16c1c
Camilo Culpian <camiloculpian@gmail.com>
2024-03-29 10:08:24 -0300

    modified:   backend/src/tickets/entities/ticket.entity.ts 	modified:   backend/src/users/entities/user.entity.ts

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -36,7 +36,7 @@ export class User {
     createdTickets: Ticket[];
 
     @OneToMany(type => Ticket, ticket => ticket.createdByUser) //All tickets asigned by this user to other users
-    reAsignedTickets: Ticket[];
+    asignedToTickets: Ticket[];
 
     @OneToMany(type => Ticket, ticket => ticket.createdByUser) //All tickets asigned to this user
     asignedTickets: Ticket[];
