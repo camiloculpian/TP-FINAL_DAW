a39c347b9a53ee68836a11e69f5bb1b809f1e206
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-03-28 22:30:18 -0300

    Nombre y Apellido

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h3>APP V1 - DAW</h3> <p>Created By:</p><p>&emsp;Camilo Culpian.<br>&emsp;Emiliano Jara.<br>&emsp;Natasha.<br>&emsp;Gaston</p>';
+    return '<h3>APP V1 - DAW</h3> <p>Created By:</p><p>&emsp;Camilo Culpian.<br>&emsp;Emiliano Jara.<br>&emsp;Natasha Szmagalski Koroll.<br>&emsp;Gaston</p>';
   }
 }
