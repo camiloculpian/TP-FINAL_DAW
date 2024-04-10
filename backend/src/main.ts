ca3916fcf99d52a2f07d343d68457f00c3d64a0b
Gaston2023 <gaz@live.com.ar>
2024-04-09 23:54:42 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,49 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.setGlobalPrefix('api/v1');
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform:true
+//     })
+//   );
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+// main modificado.ts v1.1
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración previa
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform:true
-    })
+      transform: true,
+    }),
   );
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Ejemplo API')
+    .setDescription('Descripción de la API: pruebas de CRUD')
+    .setVersion('1.0')
+    .addTag('Usuarios')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
