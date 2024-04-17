071cd0b94505b10d71a9aa5f4bbe964d1e785031
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-04-16 21:35:58 -0300

    modified:   backend/src/tickets/dto/update-ticket.dto.ts

--- backend/src/tickets/dto/update-ticket.dto.ts
+++ backend/src/tickets/dto/update-ticket.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Transform } from "class-transformer";
 import { CreateTicketDto } from './create-ticket.dto';
+import { IsBoolean, IsDateString, IsEnum, IsOptional, IsString } from "class-validator";
+import { TicketPriority, TicketsService, TicketStatus } from "../entities/ticket.entity";
 
-export class UpdateTicketDto extends PartialType(CreateTicketDto) {}
+export class UpdateTicketDto extends PartialType(CreateTicketDto) {
+    @Transform(({value}) => value.trim())
+    @IsString()
+    @IsOptional()
+    title?: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsEnum(TicketPriority)
+    @IsOptional()
+    priority?: TicketPriority;
+
+    @IsEnum(TicketsService)
+    @IsOptional()
+    service?: TicketsService;
+
+    @IsOptional()
+    assignedToUserId?: number;
+
+    image?: string;
+}
