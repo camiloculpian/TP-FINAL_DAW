ddf44932d7b1fce1f428e4251dc29dc7a8e0e1d8
camiloculpian <camiloculpian@gmail.com>
2024-04-25 19:51:44 -0300

    renamed:    backend/src/audit/tickets-audit.controller.ts -> backend/src/audit/ticket-audits.controller.ts 	new file:   backend/src/audit/ticket-audits.module.ts

---
+++ backend/src/audit/ticket-audits.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TicketAudit } from "./entities/ticket-audits.entity";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([TicketAudit]),],
+    // controllers: [TicketAuditsController],
+    // providers: [TicketAuditsService],
+    exports: [TypeOrmModule],
+  })
+  export class UsersModule {}
