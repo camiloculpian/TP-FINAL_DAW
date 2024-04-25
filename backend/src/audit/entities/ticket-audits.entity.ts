b5545ac5655f2cffdfb0fa821e33fda4a0ecf5b5
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-04-25 20:06:33 -0300

    modified:   backend/src/audit/entities/ticket-audits.entity.ts

--- backend/src/audit/entities/ticket-audits.entity.ts
+++ backend/src/audit/entities/ticket-audits.entity.ts
@@ -1,7 +1,74 @@
 import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn, Column } from "typeorm";
 import { Ticket } from "src/tickets/entities/ticket.entity";
 import { User } from "../../users/entities/user.entity";
+
+export enum TicketPriority{  // No se si ira asi #duda
+    LOW = "LOW",
+    MEDIUM = "MEDIUM",
+    HIGH = "HIGH"
+}
+
+export enum TicketStatus{
+    OPEN = "OPEN",
+    IN_PROGRESS = "IN_PROGRESS",
+    RESOLVED = "RESOLVED"
+}
+
 // FALTA HACER MODULE Y SERVICE
+@Entity()
+export class TicketAudit {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => Ticket, ticket => ticket.id)
+    @JoinColumn({ name: "id" }) // TICKET_ID
+    ticket: Ticket;
+
+    @Column()
+    description: string;
+    
+    @Column({ name: "currentUserId" })
+    currentUserId: number;
+    
+    @Column({
+        type: "enum",
+        enum: TicketPriority,
+        default: TicketPriority.LOW,
+    })
+    priority: TicketPriority;
+
+    @ManyToOne(() => User, user => user.id)
+    @JoinColumn({ name: "modifiedBy" })
+    modifiedBy: User;
+
+    @Column({ type: "date", nullable: true })
+    modifiedDate: Date;
+
+    @Column({
+        type: "enum",
+        enum: TicketStatus,
+        default: TicketStatus.OPEN,
+    })
+    status: TicketStatus;
+
+    @ManyToOne(() => User, user => user.id)
+    @JoinColumn({ name: "createdBy" })
+    createdBy: User
+
+    @Column()
+    createdAt: Date;
+
+    @Column()
+    updatedAt: Date;
+}
+
+/*
+
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn, Column } from "typeorm";
+import { Ticket } from "src/tickets/entities/ticket.entity";
+import { User } from "../../users/entities/user.entity";
+
+// FALTA HACER MODULE Y SERVICE 
 
 @Entity()
 export class TicketAudit {
@@ -26,3 +93,5 @@ export class TicketAudit {
     @Column()
     updatedAt: Date;
 }
+
+*/
