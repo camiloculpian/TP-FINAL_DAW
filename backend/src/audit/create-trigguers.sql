aa66d55628cd88624aab2406417fff33ac1b1b1a
camiloculpian <camiloculpian@gmail.com>
2024-04-26 20:21:00 -0300

    new file:   backend/src/audit/create-trigguers.sql

---
+++ backend/src/audit/create-trigguers.sql
@@ -0,0 +1,61 @@
+DROP TRIGGER trigger_ticket_update;
+
+DELIMITER //
+
+CREATE TRIGGER trigger_ticket_update
+AFTER UPDATE ON ticket
+FOR EACH ROW
+BEGIN
+    INSERT INTO ticket_audit (
+        ticketId,
+        description,
+        asignedToUserId,
+        modifiedByUserId,
+        priority,
+        status,
+        modifiedAt,
+        operation
+    )
+    VALUES (
+        NEW.id,
+        NEW.description,
+        NEW.asignedToUserId,
+        NEW.lastModifiedByUserId,
+        NEW.priority,
+        NEW.status,
+        CURRENT_DATE(),
+        'UPDATE'
+    );
+END;
+//
+
+DROP TRIGGER trigger_ticket_create;
+
+DELIMITER //
+
+CREATE TRIGGER trigger_ticket_create
+AFTER INSERT ON ticket
+FOR EACH ROW
+BEGIN
+    INSERT INTO ticket_audit (
+        ticketId,
+        description,
+        asignedToUserId,
+        modifiedByUserId,
+        priority,
+        status,
+        modifiedAt,
+        operation
+    )
+    VALUES (
+        NEW.id,
+        NEW.description,
+        NEW.asignedToUserId,
+        NEW.lastModifiedByUserId,
+        NEW.priority,
+        NEW.status,
+        CURRENT_DATE(),
+        'CREATE'
+    );
+END;
+//
