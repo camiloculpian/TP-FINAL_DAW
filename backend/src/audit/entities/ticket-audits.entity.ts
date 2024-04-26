cfa3f2d6646619f256dfd4d61cbc2dc759cf0794
camiloculpian <camiloculpian@gmail.com>
2024-04-26 20:15:42 -0300

    modified:   backend/src/audit/entities/ticket-audits.entity.ts

--- backend/src/audit/entities/ticket-audits.entity.ts
+++ backend/src/audit/entities/ticket-audits.entity.ts
@@ -9,6 +9,68 @@ export enum Operation {
     DETETE= "DELETE"
 }
 
+// DROP TRIGGER trigger_ticket_update;
+
+// DELIMITER //
+
+// CREATE TRIGGER trigger_ticket_update
+// AFTER UPDATE ON ticket
+// FOR EACH ROW
+// BEGIN
+//     INSERT INTO ticket_audit (
+//         ticketId,
+//         description,
+//         asignedToUserId,
+//         modifiedByUserId,
+//         priority,
+//         status,
+//         modifiedAt,
+//         operation
+//     )
+//     VALUES (
+//         NEW.id,
+//         NEW.description,
+//         NEW.asignedToUserId,
+//         NEW.lastModifiedByUserId,
+//         NEW.priority,
+//         NEW.status,
+//         CURRENT_DATE(),
+//         'UPDATE'
+//     );
+// END;
+// //
+
+// DROP TRIGGER trigger_ticket_create;
+
+// DELIMITER //
+
+// CREATE TRIGGER trigger_ticket_create
+// AFTER INSERT ON ticket
+// FOR EACH ROW
+// BEGIN
+//     INSERT INTO ticket_audit (
+//         ticketId,
+//         description,
+//         asignedToUserId,
+//         modifiedByUserId,
+//         priority,
+//         status,
+//         modifiedAt,
+//         operation
+//     )
+//     VALUES (
+//         NEW.id,
+//         NEW.description,
+//         NEW.asignedToUserId,
+//         NEW.lastModifiedByUserId,
+//         NEW.priority,
+//         NEW.status,
+//         CURRENT_DATE(),
+//         'CREATE'
+//     );
+// END;
+// //
+
 @Entity()
 export class TicketAudit {
     @PrimaryGeneratedColumn()
