c7f65ec253f4144c0c235863f68896def375c7e9
camiloculpian <camiloculpian@gmail.com>
2024-04-26 18:27:45 -0300

    modified:   backend/src/auth/auth.controller.ts

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -11,25 +11,25 @@ import { extname } from 'path';
 export class AuthController {
     constructor(private readonly authService: AuthService){}
 
-    @Post('register')
-    @UseInterceptors(FileInterceptor('profilePicture', {
-        storage: diskStorage({
-            destination: './uploads/profiles',
-            filename: (req, file, cb) => {
-                const randomName = Array(32).fill(null).map(()=>(Math.round(Math.random()*16)).toString(16)).join('');
-                return cb(null, `${randomName}${extname(file.originalname)}`);
-            },
-        }),
-    }))
-    register(
-        @Body() registerUserDto: RegisterUserDto,
-        @UploadedFile() file: Express.Multer.File,
-    ){
-        if(file){
-            registerUserDto.profilePicture = file.filename;
-        }
-        return this.authService.register(registerUserDto);
-    }
+    // @Post('register')
+    // @UseInterceptors(FileInterceptor('profilePicture', {
+    //     storage: diskStorage({
+    //         destination: './uploads/profiles',
+    //         filename: (req, file, cb) => {
+    //             const randomName = Array(32).fill(null).map(()=>(Math.round(Math.random()*16)).toString(16)).join('');
+    //             return cb(null, `${randomName}${extname(file.originalname)}`);
+    //         },
+    //     }),
+    // }))
+    // register(
+    //     @Body() registerUserDto: RegisterUserDto,
+    //     @UploadedFile() file: Express.Multer.File,
+    // ){
+    //     if(file){
+    //         registerUserDto.profilePicture = file.filename;
+    //     }
+    //     return this.authService.register(registerUserDto);
+    // }
 
     @Post('login')
     login(
