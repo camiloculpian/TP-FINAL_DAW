3ab2a9f38352b15c991bd2df0db06fb9252e0eae
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-04-26 21:00:16 -0300

    modified:   backend/src/tickets/tickets.controller.ts

--- backend/src/tickets/tickets.controller.ts
+++ backend/src/tickets/tickets.controller.ts
@@ -1,63 +1,29 @@
-// import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
-// import { TicketsService } from './tickets.service';
-// import { CreateTicketDto } from './dto/create-ticket.dto';
-// import { UpdateTicketDto } from './dto/update-ticket.dto';
-// import { FileInterceptor } from '@nestjs/platform-express';
-// import { diskStorage } from 'multer';
-// import { extname } from 'path';
-
-// @Controller('tickets')
-// export class TicketsController {
-//   constructor(private readonly ticketsService: TicketsService) {}
-
-//   @Post()
-//   @UseInterceptors(FileInterceptor('image', {
-//     storage: diskStorage({
-//       destination: './src/uploads/ticket/img',
-//       filename: (req, file, cb) => {
-//         const randomName = Array(32).fill(null).map(()=>(Math.round(Math.random()*16)).toString(16)).join('');
-//         return cb(null, `${randomName}${extname(file.originalname)}`);
-//       },
-//     }),
-//   }))
-//   create(@Body() createTicketDto: CreateTicketDto, @UploadedFile() file: Express.Multer.File){
-//     if(file){
-//       createTicketDto.image = file.filename;
-//     }
-//     if(!createTicketDto.image){
-//       delete createTicketDto.image;
-//     }
-//     return this.ticketsService.create(createTicketDto);
-//   }
-
-//   @Get()
-//   findAll() {
-//     return this.ticketsService.findAll();
-//   }
-
-//   @Get(':id')
-//   findOne(@Param('id') id: string) {
-//     return this.ticketsService.findOne(+id);
-//   }
-
-//   @Patch(':id')
-//   update(@Param('id') id: string, @Body() updateTicketDto: UpdateTicketDto) {
-//     return this.ticketsService.update(+id, updateTicketDto);
-//   }
-
-//   @Delete(':id')
-//   remove(@Param('id') id: string) {
-//     return this.ticketsService.remove(+id);
-//   }
-// }
-
-
-// tickets modified v1.1
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, UseGuards, NotFoundException, Inject } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UseInterceptors,
+    UploadedFile,
+    UseGuards,
+    NotFoundException,
+    Inject,
+    BadRequestException,
+    InternalServerErrorException,
+} from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
+import {
+    ApiTags,
+    ApiOperation,
+    ApiResponse,
+    ApiBody,
+    ApiParam,
+} from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
@@ -70,140 +36,186 @@ import { UsersService } from 'src/users/users.service';
 @ApiTags('Tickets')
 @Controller('tickets')
 export class TicketsController {
-  constructor(private readonly ticketsService: TicketsService) { }
-  @Inject(UsersService)
-    private readonly userService: UsersService
+    constructor(private readonly ticketsService: TicketsService) { }
+    @Inject(UsersService)
+    private readonly userService: UsersService;
 
-  @Post()
-  // CREA UN TICKET
-  @UseGuards(AuthGuard)
-  @Roles(Role.ADMIN)
-  @ApiOperation({ summary: 'Crear un nuevo ticket' })
-  @ApiResponse({ status: 201, description: 'Ticket creado con éxito' })
-  @ApiResponse({ status: 400, description: 'Datos inválidos' })
-  @UseInterceptors(
-      FileInterceptor('archive', {
-          storage: diskStorage({
-              destination: (req, file, cb) => {
-                  // Verifica la extension y lo manda a su carpeta
-                  const ext = extname(file.originalname).toLowerCase();
-                  if (ext === '.pdf') {
-                      cb(null, './src/uploads/ticket/pdf');
-                  } else if (['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
-                      cb(null, './src/uploads/ticket/image');
-                  } else {
-                      cb(new Error('Tipo de archivo no soportado'), '');
-                  }
-              },
-              filename: (req, file, cb) => {
-                  const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
-                  cb(null, `${randomName}${extname(file.originalname)}`);
-              },
-          }),
-          fileFilter: (req, file, cb) => {
-              // Filtros
-              const ext = extname(file.originalname).toLowerCase();
-              if (ext === '.pdf' || ['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
-                  cb(null, true);
-              } else {
-                  cb(null, false);
-              }
-          },
-      })
-  )
-  async create(
-      @Body() createTicketDto: CreateTicketDto,
-      @UploadedFile() archive: Express.Multer.File,
-      @CurrentUser('sub') userId: number
-  ) {
-      if (archive) {
-          createTicketDto.archive = archive.filename;
-      }
-      const newTicket = await this.ticketsService.create(createTicketDto, userId);
-      return newTicket;
-  }
-  
-  @Get()
-  // OBTIENE TODOS LOS TICKETS DE UN USUARIO
-  @UseGuards(AuthGuard)
-  @ApiOperation({ summary: 'Obtener todos los tickets' })
-  @ApiResponse({ status: 200, description: 'Lista de tickets' })
-  findAll( @CurrentUser('sub') userId: number) {
-    console.log(userId); // ACA TENGO LOS DATOS DEL USUARIO
-    return this.ticketsService.findAll(userId);
-  }
+    // Crea un ticket: solo usuarios ADMIN
+    @Post()
+    @UseGuards(AuthGuard)
+    @Roles(Role.ADMIN)
+    @ApiOperation({ summary: 'Crear un nuevo ticket' })
+    @ApiResponse({ status: 201, description: 'Ticket creado con éxito' })
+    @ApiResponse({ status: 400, description: 'Datos inválidos' })
+    @UseInterceptors(
+        FileInterceptor('archive', {
+            storage: diskStorage({
+                destination: (req, file, cb) => {
+                    // Verifica la extension y lo manda a su carpeta
+                    const ext = extname(file.originalname).toLowerCase();
+                    if (ext === '.pdf') {
+                        cb(null, './src/uploads/ticket/pdf');
+                    } else if (['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
+                        cb(null, './src/uploads/ticket/image');
+                    } else {
+                        cb(new Error('Tipo de archivo no soportado'), '');
+                    }
+                },
+                filename: (req, file, cb) => {
+                    const randomName = Array(32)
+                        .fill(null)
+                        .map(() => Math.round(Math.random() * 16).toString(16))
+                        .join('');
+                    cb(null, `${randomName}${extname(file.originalname)}`);
+                },
+            }),
+            fileFilter: (req, file, cb) => {
+                // Filtros
+                const ext = extname(file.originalname).toLowerCase();
+                if (ext === '.pdf' || ['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
+                    cb(null, true);
+                } else {
+                    cb(null, false);
+                }
+            },
+        }),
+    )
+    async create(
+        @Body() createTicketDto: CreateTicketDto,
+        @UploadedFile() archive: Express.Multer.File,
+        @CurrentUser('sub') userId: number,
+    ) {
+        try {
+            if (archive) {
+                createTicketDto.archive = archive.filename;
+            }
+            const newTicket = await this.ticketsService.create(
+                createTicketDto,
+                userId,
+            );
+            return newTicket;
+        } catch (error) {
+            console.error('Error creating ticket:', error);
+            throw new BadRequestException('Error creating ticket');
+        }
+    }
 
-  @Get(':id')
-  @UseGuards(AuthGuard)
-  @ApiOperation({ summary: 'Obtener un ticket por ID' })
-  @ApiResponse({ status: 200, description: 'Ticket encontrado' })
-  @ApiResponse({ status: 404, description: 'Ticket no encontrado' })
-  @ApiParam({ name: 'id', description: 'ID único del ticket' })
-  findOne(@Param('id') id: string, @CurrentUser('sub') userId: number) {
-    return this.ticketsService.findOne(+id, +userId);
-  }
+    // Obtiene todos los tickets de un usuario
+    @Get()
+    @UseGuards(AuthGuard)
+    @ApiOperation({ summary: 'Obtener todos los tickets' })
+    @ApiResponse({ status: 200, description: 'Lista de tickets' })
+    async findAll(@CurrentUser('sub') userId: number) {
+        try {
+            return await this.ticketsService.findAll(userId);
+        } catch (error) {
+            console.error('Error fetching tickets:', error);
+            throw new InternalServerErrorException('Failed to fetch tickets');
+        }
+    }
 
-  @Patch(':id')
-  // Actualiza un ticket
-  @UseGuards(AuthGuard)
-  @Roles(Role.USER, Role.ADMIN)
-  @ApiOperation({ summary: 'Actualizar un ticket' })
-  @ApiResponse({ status: 200, description: 'Ticket actualizado con éxito' })
-  @ApiResponse({ status: 404, description: 'Ticket no encontrado' })
-  @ApiParam({ name: 'id', description: 'ID único del ticket' })
-  @UseInterceptors(
-      FileInterceptor('archive', {
-          storage: diskStorage({
-              destination: (req, file, cb) => {
-                  const ext = extname(file.originalname).toLowerCase();
-                  if (ext === '.pdf') {
-                      cb(null, './src/uploads/ticket/pdf');
-                  } else if (['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
-                      cb(null, './src/uploads/ticket/image');
-                  } else {
-                      cb(new Error('Tipo de archivo no soportado'), '');
-                  }
-              },
-              filename: (req, file, cb) => {
-                  const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
-                  cb(null, `${randomName}${extname(file.originalname)}`);
-              },
-          }),
-          fileFilter: (req, file, cb) => {
-              const ext = extname(file.originalname).toLowerCase();
-              if (ext === '.pdf' || ['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
-                  cb(null, true);
-              } else {
-                  cb(null, false);
-              }
-          },
-      })
-  )
-  async update(
-      @Param('id') id: string,
-      @Body() updateTicketDto: UpdateTicketDto,
-      @UploadedFile() archive: Express.Multer.File,
-      @CurrentUser('sub') userId: number
-  ) {
-      if (archive) {
-          updateTicketDto.archive = archive.filename;
-      }
-      
-      const updatedTicket = await this.ticketsService.update(+id, updateTicketDto, userId);
-  
-      return updatedTicket;
-  }
+    // Buscar tickets
+    @Get(':id')
+    @UseGuards(AuthGuard)
+    @ApiOperation({ summary: 'Obtener un ticket por ID' })
+    @ApiResponse({ status: 200, description: 'Ticket encontrado' })
+    @ApiResponse({ status: 404, description: 'Ticket no encontrado' })
+    @ApiParam({ name: 'id', description: 'ID único del ticket' })
+    async findOne(@Param('id') id: string, @CurrentUser('sub') userId: number) {
+        try {
+            const ticket = await this.ticketsService.findOne(+id, userId);
+            if (!ticket) {
+                throw new NotFoundException('Ticket not found');
+            }
+            return ticket;
+        } catch (error) {
+            console.error(`Error fetching ticket with ID ${id}:`, error);
+            throw new NotFoundException('Ticket not found');
+        }
+    }
 
-  @Delete(':id')
-  // Elimina un ticket
-  @UseGuards(AuthGuard)
-  @Roles(Role.ADMIN)
-  @ApiOperation({ summary: 'Eliminar un ticket' })
-  @ApiResponse({ status: 200, description: 'Ticket eliminado con éxito' })
-  @ApiResponse({ status: 404, description: 'Ticket no encontrado' })
-  @ApiParam({ name: 'id', description: 'ID único del ticket' })
-  remove(@Param('id') id: string) {
-    return this.ticketsService.remove(+id);
-  }
+    // Actualiza los tickets: un usuario ADMIN puede actualizar TODOS los campos y un USER solo descripcion, archive y status (REVISAR!)
+    @Patch(':id')
+    @UseGuards(AuthGuard)
+    @Roles(Role.USER, Role.ADMIN)
+    @ApiOperation({ summary: 'Actualizar un ticket' })
+    @ApiResponse({ status: 200, description: 'Ticket actualizado con éxito' })
+    @ApiResponse({ status: 404, description: 'Ticket no encontrado' })
+    @ApiParam({ name: 'id', description: 'ID único del ticket' })
+    @UseInterceptors(
+        FileInterceptor('archive', {
+            storage: diskStorage({
+                destination: (req, file, cb) => {
+                    const ext = extname(file.originalname).toLowerCase();
+                    if (ext === '.pdf') {
+                        cb(null, './src/uploads/ticket/pdf');
+                    } else if (['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
+                        cb(null, './src/uploads/ticket/image');
+                    } else {
+                        cb(new Error('Tipo de archivo no soportado'), '');
+                    }
+                },
+                filename: (req, file, cb) => {
+                    const randomName = Array(32)
+                        .fill(null)
+                        .map(() => Math.round(Math.random() * 16).toString(16))
+                        .join('');
+                    cb(null, `${randomName}${extname(file.originalname)}`);
+                },
+            }),
+            fileFilter: (req, file, cb) => {
+                const ext = extname(file.originalname).toLowerCase();
+                if (ext === '.pdf' || ['.jpg', '.jpeg', '.png', '.gif'].includes(ext)) {
+                    cb(null, true);
+                } else {
+                    cb(null, false);
+                }
+            },
+        }),
+    )
+    async update(
+        @Param('id') id: string,
+        @Body() updateTicketDto: UpdateTicketDto,
+        @UploadedFile() archive: Express.Multer.File,
+        @CurrentUser('sub') userId: number,
+    ) {
+        try {
+            if (archive) {
+                updateTicketDto.archive = archive.filename;
+            }
+            const updatedTicket = await this.ticketsService.update(
+                +id,
+                updateTicketDto,
+                userId,
+            );
+            if (!updatedTicket) {
+                throw new NotFoundException('Ticket not found');
+            }
+            return updatedTicket;
+        } catch (error) {
+            console.error(`Error updating ticket with ID ${id}:`, error);
+            throw new InternalServerErrorException('Failed to update ticket');
+        }
+    }
+
+    // Elimina un ticket: solo ADMIN
+    @Delete(':id')
+    @UseGuards(AuthGuard)
+    @Roles(Role.ADMIN)
+    @ApiOperation({ summary: 'Eliminar un ticket' })
+    @ApiResponse({ status: 200, description: 'Ticket eliminado con éxito' })
+    @ApiResponse({ status: 404, description: 'Ticket no encontrado' })
+    @ApiParam({ name: 'id', description: 'ID único del ticket' })
+    async remove(@Param('id') id: string) {
+        try {
+            const removedTicket = await this.ticketsService.remove(+id);
+            if (!removedTicket) {
+                throw new NotFoundException('Ticket not found');
+            }
+            return removedTicket;
+        } catch (error) {
+            console.error(`Error removing ticket with ID ${id}`, error);
+            throw new NotFoundException('Ticket not found');
+        }
+    }
 }
-
