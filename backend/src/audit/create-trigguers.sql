7bd03e46950e66221ca8ee3ac97408574ccb346d
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-04-27 19:09:45 -0300

    modified:   backend/src/audit/create-trigguers.sql 	modified:   backend/src/audit/ticket-audits.controller.ts

--- backend/src/audit/create-trigguers.sql
+++ backend/src/audit/create-trigguers.sql
@@ -59,36 +59,3 @@ BEGIN
 END;
 //
 DELIMITER ;
-
-DROP TRIGGER IF EXISTS trigger_ticket_delete;
-
-DELIMITER //
-CREATE TRIGGER IF NOT EXISTS trigger_ticket_delete
-AFTER DELETE ON ticket
-FOR EACH ROW
-BEGIN
-    INSERT INTO ticket_audit (
-        ticketId,
-        description,
-        asignedToUserId,
-        modifiedByUserId,
-        priority,
-        status,
-        modifiedAt,
-        operation
-    )
-    VALUES (
-        OLD.id,
-        OLD.description,
-        OLD.asignedToUserId,
-        OLD.lastModifiedByUserId,
-        OLD.priority,
-        OLD.status,
-        CURRENT_DATE(),
-        'DELETE'
-    );
-END;
-//
-DELIMITER ;
-
---- Cuando se elimina un ticket deberia aparecer DELETE y aparece UPDATE ---
--- backend/src/audit/ticket-audits.controller.ts
+++ backend/src/audit/ticket-audits.controller.ts
@@ -14,8 +14,6 @@ import { AuthGuard } from 'src/auth/auth.guard';
 import { Role } from 'src/auth/enums/role.enum';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 
-// FALTA HACER MODULE Y SERVICE
-//import { TicketsAuditService } from "./tickets-audit.service";
 
 @ApiTags('Ticket Audits')
 @Controller('ticket-audits')
@@ -44,60 +42,3 @@ export class TicketAuditsController {
         }
     }
 }
-
-
-// CODE MODIFIED V1.1
-// import { Controller, Get, Post, Body, UseGuards, Param, Delete, Patch } from "@nestjs/common";
-// import { ApiTags, ApiOperation, ApiResponse } from "@nestjs/swagger";
-// import { TicketAuditsService } from "./ticket-audits.service";
-// import { AuthGuard } from "src/auth/auth.guard";
-// import { Role } from "src/auth/enums/role.enum";
-// import { Roles } from "src/auth/decorators/roles.decorator";
-// import { TicketAuditDto } from "src/audit/dto/ticket-audit-dto";
-
-// @ApiTags("Ticket Audits")
-// @Controller("ticket-audits")
-// export class TicketAuditsController {
-//     constructor(private readonly ticketAuditsService: TicketAuditsService) { }
-
-//     @Get(':id')
-//     @ApiOperation({ summary: "Obtener todos los registros de auditoría de tickets" })
-//     @ApiResponse({ status: 200, description: "Lista de registros de auditoría" })
-
-//     @UseGuards(AuthGuard)
-//     @Roles(Role.ADMIN)
-//     findAll(@Param('id') id: number) {
-//         return this.ticketAuditsService.findAll(id);
-//     }
-
-//     @Post()
-//     @ApiOperation({ summary: "Crear un nuevo registro de auditoría de ticket" })
-//     @ApiResponse({ status: 201, description: "Registro de auditoría creado con éxito" })
-
-//     @UseGuards(AuthGuard)
-//     @Roles(Role.ADMIN)
-//     async create(@Body() ticketAuditDto: TicketAuditDto) {
-//         return await this.ticketAuditsService.create(ticketAuditDto);
-//     }
-
-//     @Patch(':id')
-//     @ApiOperation({ summary: "Actualizar un registro de auditoría de ticket" })
-//     @ApiResponse({ status: 200, description: "Registro de auditoría actualizado con éxito" })
-
-//     @UseGuards(AuthGuard)
-//     @Roles(Role.ADMIN)
-//     async update(@Param('id') id: string, @Body() ticketAuditDto: TicketAuditDto) {
-//         return await this.ticketAuditsService.update(parseInt(id), ticketAuditDto);
-//     }
-
-//     @Delete(':id')
-//     @ApiOperation({ summary: "Eliminar un registro de auditoría de ticket" })
-//     @ApiResponse({ status: 200, description: "Registro de auditoría eliminado con éxito" })
-
-//     @UseGuards(AuthGuard)
-//     @Roles(Role.ADMIN)
-//     async remove(@Param('id') id: string) {
-//         return await this.ticketAuditsService.remove(parseInt(id));
-//     }
- 
-// }
