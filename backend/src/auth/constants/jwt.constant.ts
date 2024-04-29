c6a56965cc333aa3be6fc53fdaeab481233cfac1
camiloculpian <camiloculpian@gmail.com>
2024-04-28 21:58:23 -0300

    modified:   backend/src/auth/constants/jwt.constant.ts 	new file:   backend/src/config/jwtConstants.env

--- backend/src/auth/constants/jwt.constant.ts
+++ backend/src/auth/constants/jwt.constant.ts
@@ -1,3 +1,10 @@
+import { ConfigModule } from "@nestjs/config";
+
+ConfigModule.forRoot({
+    envFilePath: 'src/config/jwtConstants.env',
+  });
+  
 export const jwtConstants= {
-    secret:'sdfkñlujhsa87654iklhgfas974yt5qaitgaso8i745yalihjgbfsol9i8475yaiugblei444875qe,.m,'
+    // secret:'sdfkñlujhsa87654iklhgfas974yt5qaitgaso8i745yalihjgbfsol9i8475yaiugblei444875qe,.m,'
+    secret: process.env.SECRET
 }
