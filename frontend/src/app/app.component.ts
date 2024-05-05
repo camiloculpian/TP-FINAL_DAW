b14298ec0564a96c538e00a6667296997a96ebd1
camiloculpian <camiloculpian@gmail.com>
2024-05-04 22:58:54 -0300

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/app/app.component.html 	modified:   frontend/src/app/app.component.ts 	modified:   frontend/src/app/notify/notify.component.css 	modified:   frontend/src/app/notify/notify.component.html 	modified:   frontend/src/index.html 	modified:   frontend/src/styles.css

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotifyComponent } from './notify/notify.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotifyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
