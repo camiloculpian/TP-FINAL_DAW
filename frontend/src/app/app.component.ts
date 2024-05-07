cee6e3c8307417a32886c5695294cdd96ac2ccaa
camiloculpian <camiloculpian@gmail.com>
2024-05-07 18:16:17 -0300

    modified:   frontend/src/app/app.component.ts 	new file:   frontend/src/app/layout/app-layout/app-layout.component.css 	new file:   frontend/src/app/layout/app-layout/app-layout.component.html 	new file:   frontend/src/app/layout/app-layout/app-layout.component.spec.ts 	new file:   frontend/src/app/layout/app-layout/app-layout.component.ts 	new file:   frontend/src/app/layout/login-layout/login-layout.component.css 	new file:   frontend/src/app/layout/login-layout/login-layout.component.html 	new file:   frontend/src/app/layout/login-layout/login-layout.component.spec.ts 	new file:   frontend/src/app/layout/login-layout/login-layout.component.ts 	new file:   frontend/src/app/layout/main-layout/main-layout.component.css 	new file:   frontend/src/app/layout/main-layout/main-layout.component.html 	new file:   frontend/src/app/layout/main-layout/main-layout.component.spec.ts 	new file:   frontend/src/app/layout/main-layout/main-layout.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NotifyComponent } from './modules/notify/notify.component';;
+import { NotifyComponent } from './components/notify/notify.component';;
 import { HomeComponent } from './pages/home/home.component';
 
 @Component({
