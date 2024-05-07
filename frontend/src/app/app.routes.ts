b9f545ddc8ae4acee7de14fb32c8c727a0fb1d4a
camiloculpian <camiloculpian@gmail.com>
2024-05-07 18:35:06 -0300

    modified:   frontend/src/app/app.component.html 	modified:   frontend/src/app/app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginLayoutComponent } from './layout/login-layout/login-layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'login', component: LoginLayoutComponent }
 ];
