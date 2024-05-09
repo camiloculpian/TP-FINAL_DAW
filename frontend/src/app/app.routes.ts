2cf045d8e2e7d371073d2a37d26ac60d50f63fda
camiloculpian <camiloculpian@gmail.com>
2024-05-09 20:34:27 -0300

    modified:   frontend/src/app/app.routes.ts 	new file:   frontend/src/app/components/audits/audits.component.css 	new file:   frontend/src/app/components/audits/audits.component.html 	new file:   frontend/src/app/components/audits/audits.component.spec.ts 	new file:   frontend/src/app/components/audits/audits.component.ts 	new file:   frontend/src/app/components/tickets/tickets.component.css 	new file:   frontend/src/app/components/tickets/tickets.component.html 	new file:   frontend/src/app/components/tickets/tickets.component.spec.ts 	new file:   frontend/src/app/components/tickets/tickets.component.ts 	new file:   frontend/src/app/components/users/users.component.css 	new file:   frontend/src/app/components/users/users.component.html 	new file:   frontend/src/app/components/users/users.component.spec.ts 	new file:   frontend/src/app/components/users/users.component.ts 	modified:   frontend/src/app/layout/app-layout/app-layout.component.html 	modified:   frontend/src/app/layout/app-layout/app-layout.component.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,20 @@ import { LoginLayoutComponent } from './layout/login-layout/login-layout.compone
 import { HomeComponent } from './pages/home/home.component';
 import { NotifyComponent } from './components/notify/notify.component';
 import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+import { UsersComponent } from './components/users/users.component';
+import { AuditsComponent } from './components/audits/audits.component';
 
 export const routes: Routes = [
     
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginLayoutComponent },
-    { path: 'app', component: AppLayoutComponent },
+    { path: 'app', component: AppLayoutComponent,
+        children: [
+            {path: 'users',component: UsersComponent},
+            {path: 'tickets',component: TicketsComponent},
+            {path: 'audits',component: AuditsComponent}
+        ]
+    },
     { path: 'notify', component: NotifyComponent}
 ];
--- frontend/src/app/layout/app-layout/app-layout.component.ts
+++ frontend/src/app/layout/app-layout/app-layout.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../../components/login/login.service';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { StatusBarComponent } from '../../components/status-bar/status-bar.component';
 import { AppMenuComponent } from '../../components/app-menu/app-menu.component';
 
 @Component({
   selector: 'app-app-layout',
   standalone: true,
-  imports: [StatusBarComponent, AppMenuComponent],
+  imports: [RouterOutlet, StatusBarComponent, AppMenuComponent],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.css'
 })
