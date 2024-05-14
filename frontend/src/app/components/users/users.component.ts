4a87db8657e00887e80e6bd58c5fddc97db975fa
camiloculpian <camiloculpian@gmail.com>
2024-05-14 17:54:20 -0300

    modified:   frontend/src/app/components/users/users.component.ts

--- frontend/src/app/components/users/users.component.ts
+++ frontend/src/app/components/users/users.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/users';
+import { Response } from '../../models/responses';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +10,35 @@ import { Component } from '@angular/core';
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
+  public usersList:JSON|undefined;
+  public response: Response|null;
+
+  constructor(private _httpReq: HttpClient) {
+    this.response = null;
+    this.usersList = undefined;
+   }
+
+  ngOnInit(): void {
+    console.log('ngOnInit()');
+    let user:User = JSON.parse(String(localStorage.getItem('user')));
+    this._httpReq.get<Response>("http://localhost:3000/api/v1/users",
+    {
+      headers: new HttpHeaders ({   
+          "Authorization": String("Bearer "+user.token),
+      }),
+    }).subscribe({
+      next: (resp => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            this.response = resp;
+            this.usersList = this.response.data;
+            console.log(this.response);
+        }),
+        error: (err)  =>{
+          this.response=err;
+          console.log(this.response);
+        } 
+      })
+  }
 
 }
