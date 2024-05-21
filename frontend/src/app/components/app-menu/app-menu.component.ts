1d4883f66fca6cfab86f2b14e97c71ee75c31d4b
camiloculpian <camiloculpian@gmail.com>
2024-05-21 18:48:54 -0300

    modified:   frontend/src/app/components/app-menu/app-menu.component.html 	modified:   frontend/src/app/components/app-menu/app-menu.component.ts

--- frontend/src/app/components/app-menu/app-menu.component.ts
+++ frontend/src/app/components/app-menu/app-menu.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-app-menu',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, NgIf],
   templateUrl: './app-menu.component.html',
   styleUrl: './app-menu.component.css'
 })
 export class AppMenuComponent {
-
+  public authService = inject(LoginService);
 }
