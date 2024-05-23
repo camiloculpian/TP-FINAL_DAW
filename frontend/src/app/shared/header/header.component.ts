543820af454c2ed7d39317d76527c5429d9bc599
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-05-23 17:51:42 -0300

    modified:   frontend/src/app/components/app-menu/app-menu.component.css 	modified:   frontend/src/app/components/app-menu/app-menu.component.html 	modified:   frontend/src/app/components/status-bar/status-bar.component.css 	modified:   frontend/src/app/components/status-bar/status-bar.component.html 	modified:   frontend/src/app/components/tickets/tickets.component.html 	modified:   frontend/src/app/layout/app-layout/app-layout.component.css 	modified:   frontend/src/app/layout/app-layout/app-layout.component.html 	modified:   frontend/src/app/shared/header/header.component.css 	modified:   frontend/src/app/shared/header/header.component.html 	modified:   frontend/src/app/shared/header/header.component.ts 	new file:   frontend/src/assets/img/Black White Minimalist SImple Monochrome Initial Name Logo.png 	modified:   frontend/src/index.html

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NgbNavModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,NgbCollapseModule],
+  imports: [RouterLink, NgbCollapseModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   isMenuCollapsed = true;
