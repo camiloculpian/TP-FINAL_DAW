2f36f05c5bffe11cea41cc766804f2a922ff8019
camiloculpian <camiloculpian@gmail.com>
2024-05-25 14:19:07 -0300

    modified:   frontend/src/app/components/audits/audits.service.ts

--- frontend/src/app/components/audits/audits.service.ts
+++ frontend/src/app/components/audits/audits.service.ts
@@ -4,14 +4,14 @@ import { Observable } from "rxjs";
 import { Response } from "../../models/responses";
 
 @Injectable({
-    providedIn: 'root',
-  })
-  export class AuditsService {
-    private apiUrl = 'http://localhost:3000/api/v1';
+  providedIn: 'root',
+})
+export class AuditsService {
+  private apiUrl = 'http://localhost:3000/api/v1';
 
-    constructor(private _httpReq: HttpClient) {}
+  constructor(private _httpReq: HttpClient) {}
 
-    getAudits(ticketId: number): Observable<Response> {
-        return this._httpReq.get<any>(`${this.apiUrl}/ticket-audits/${ticketId}`);
-    }
+  getAudits(ticketId: number): Observable<Response> {
+      return this._httpReq.get<any>(`${this.apiUrl}/ticket-audits/${ticketId}`);
+  }
 }
