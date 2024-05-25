c77a4d469ac3725d67ed2351d0d6b1b6c052f955
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-05-25 09:36:49 -0300

    modified:   frontend/src/app/components/login/login.component.css 	modified:   frontend/src/app/components/login/login.component.html 	modified:   frontend/src/app/components/login/login.component.ts 	modified:   frontend/src/app/layout/login-layout/login-layout.component.html 	modified:   frontend/src/app/pages/home/home.component.css 	modified:   frontend/src/app/pages/home/home.component.html 	modified:   frontend/src/app/shared/footer/footer.component.css 	modified:   frontend/src/app/shared/footer/footer.component.html 	modified:   frontend/src/app/shared/header/header.component.css 	modified:   frontend/src/app/shared/header/header.component.html

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { sha512 } from 'js-sha512';
 import { LoginService } from './login.service';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
@@ -12,58 +17,58 @@ import Swal from 'sweetalert2';
   imports: [ReactiveFormsModule, CommonModule],
   providers: [LoginService],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   message: string = '';
   userForm = new FormGroup({
     username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
+    password: new FormControl('', Validators.required),
   });
 
-  constructor(
-    private router: Router,
-    private loginService: LoginService
-  ) {}
+  constructor(private router: Router, private loginService: LoginService) {}
 
-  onUsernameChange(){
+  onUsernameChange() {
     this.message = '';
   }
 
-  onPasswordChange(){
-    if(this.userForm.value.username){
+  onPasswordChange() {
+    if (this.userForm.value.username) {
       this.message = '';
     }
   }
 
   onSubmit() {
-    if(!this.userForm.value.username){
-      this.message = 'Username no puede estar vacio';
-    } else if (!this.userForm.value.password){
-      this.message = 'Password no puede estar vacio';
+    if (!this.userForm.value.username || !this.userForm.value.password) {
+      this.showErrorMessage(
+        'Por favor, complete todos los campos antes de iniciar sesión.'
+      );
     } else {
-      this.loginService.logIn(String(this.userForm.value.username), sha512(String(this.userForm.value.password)))
-      .subscribe({
-        next: (resp) => {
-          if (resp.statusCode == 201){
-            localStorage.setItem('user', JSON.stringify(resp.data));
-            this.router.navigate(['/app']);
-          }
-        },
-        error: (err) => {
-          console.log('**ERROR! ' + JSON.stringify(err.error));
-          this.showErrorMessage(err.error.message); // Mostrar la ventana modal de error
-        }
-      });
+      this.loginService
+        .logIn(
+          String(this.userForm.value.username),
+          sha512(String(this.userForm.value.password))
+        )
+        .subscribe({
+          next: (resp) => {
+            if (resp.statusCode == 201) {
+              localStorage.setItem('user', JSON.stringify(resp.data));
+              this.router.navigate(['/app']);
+            }
+          },
+          error: (err) => {
+            console.log('**ERROR! ' + JSON.stringify(err.error));
+            this.showErrorMessage(err.error.message);
+          },
+        });
     }
   }
 
-  // Función para mostrar la ventana modal de error
   showErrorMessage(errorMessage: string) {
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
-      text: errorMessage
+      text: errorMessage,
     });
   }
 }
