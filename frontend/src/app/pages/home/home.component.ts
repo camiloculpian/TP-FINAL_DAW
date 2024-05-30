90ae743001860b6cd984ae51c3b41b9ad9e001e4
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-05-30 18:52:54 -0300

    modified:   frontend/src/app/pages/home/home.component.css 	modified:   frontend/src/app/pages/home/home.component.html 	modified:   frontend/src/app/pages/home/home.component.spec.ts 	modified:   frontend/src/app/pages/home/home.component.ts

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -9,16 +9,45 @@ import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent,NgbCarouselModule,CommonModule],
+  imports: [HeaderComponent, FooterComponent, NgbCarouselModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-
-
 export class HomeComponent {
-  images = [
-  'https://s1.1zoom.me/b4147/86/Keyboard_Toys_Macro_Closeup_Laptops_Police_520947_1920x1080.jpg',
-  'https://www.utu.edu.uy/sites/www.utu.edu.uy/files/styles/styles_galeria/public/generico/imagenes/2020-11/reparacion-pc.jpg?h=453af878&itok=eJYtAewE',
-  'https://wallpapers.com/images/featured/laptop-murjp1nk4lp1idlt.jpg'
-];
+  testimonials = [
+    {
+      quote: "¡Compuexpress salvó mi computadora! Pensé que había perdido todo, pero lograron recuperar mis archivos y mi computadora funciona mejor que nunca.",
+      author: "Homero Simpson",
+      image: "https://i.pinimg.com/originals/26/47/3a/26473aeb0510191272951312d6a3a70c.jpg",
+      rating: 5
+    },
+    {
+      quote: "El servicio de Compuexpress fue excelente. Fueron rápidos y eficientes, y ahora mi computadora funciona como nueva. ¡Gracias!",
+      author: "Marge Simpson",
+      image: "https://i.pinimg.com/564x/84/84/19/848419b365d4c421bbcb44a28b4d596b.jpg",
+      rating: 5
+    },
+    {
+      quote: "Recomiendo totalmente a Compuexpress. Tuvieron mi PC lista en menos de un día y no tuve que gastar una fortuna. ¡Grandes profesionales!",
+      author: "Lisa Simpson",
+      image: "https://i.pinimg.com/originals/0c/21/44/0c2144dce9add8529a30517efcbd36c8.jpg",
+      rating: 5
+    },
+    {
+      quote: "¡Increíble servicio! Compuexpress arregló mi computadora en tiempo récord. Ahora puedo seguir jugando sin problemas.",
+      author: "Bart Simpson",
+      image: "https://i.pinimg.com/originals/6f/e8/b1/6fe8b18d4ad105fcdc4b7c7a2f124926.png",
+      rating: 3
+    },
+    {
+      quote: "Estaba preocupada por mi computadora, pero Compuexpress la arregló rápidamente. Son muy confiables y profesionales.",
+      author: "Maggie Simpson",
+      image: "https://i.pinimg.com/736x/38/7c/67/387c676a9a0b98c3d88f216ca6b6e13b.jpg",
+      rating: 5
+    }
+  ];
+
+  generateArray(length: number): any[] {
+    return Array(length);
+  }
 }
